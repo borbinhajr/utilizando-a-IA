c194a43e0a213f9ca98d38910d332d96c4a18049
Adrian Henrique de Borba <159184989+borbinhajr@users.noreply.github.com>
2025-09-16 11:18:12 -0300

    16/08

--- script.js
+++ script.js
@@ -83,6 +83,7 @@ const perguntas = [
 
 let atual = 0;
 let perguntaAtual;
+let historiaFinal = "";
 
 function mostraPergunta(){
     perguntaAtual = perguntas[atual];
@@ -93,9 +94,24 @@ function mostraPergunta(){
     function mostraAlternativas(){
     for(const alternativa of perguntaAtual.alternativas){
     const botaoAlternativas = document.createElement("button");
-    botaoAlternativas.textContent = alternativa;
+    botaoAlternativas.textContent = alternativa.texto;
+    botaoAlternativas.addEventListener("click",() => respostaSelecionada(alternativa));
     caixaAlternativas.appendChild(botaoAlternativas);
     }
+     }
+
+     function respostaSelecionada(opcaoSelecionada){
+        const afirmacoes = opcaoSelecionada. afirmacoes;
+        historiaFinal+= afirmacoes+"";
+        atual++;
+        mostraPergunta();
+     }
+
+
+     function mostraResultado(){
+        caixaPerguntas.textContent = "Em 2049...";
+        textoResultado. textContent = historiaFinal;
+        caixaAlternativas.textContent = "";
      }
     
      mostraPergunta();
